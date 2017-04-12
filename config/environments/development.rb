8338b9249217208d9235e9a54b2e5924181a0d95
gazhrot <axel.bruneaux@epitech.eu>
2017-04-12 21:49:30 +0200

    add line for allow subdomain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-  #config.action_dispatch.tld_length = 0
+  config.action_dispatch.tld_length = 1
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
