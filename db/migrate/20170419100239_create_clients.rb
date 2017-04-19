c3465184406e774c524efa1d13096101236e7a1d
gazhrot <axel.bruneaux@epitech.eu>
2017-04-19 12:02:51 +0200

    model/migration client

---
+++ db/migrate/20170419100239_create_clients.rb
@@ -0,0 +1,14 @@
+class CreateClients < ActiveRecord::Migration[5.1]
+  def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :name
+      t.float :lat
+      t.float :lng
+      t.date :last_order_date
+      t.date :last_visit_date
+
+      t.timestamps
+    end
+  end
+end
