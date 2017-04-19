f30a2ddbd28d4bebe0579564ea4325cb2ddd102d
gazhrot <axel.bruneaux@epitech.eu>
2017-04-19 15:07:15 +0200

    define all users controller func and user model

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,40 @@
+# app/controllers/api/users_controller.rb
+module API
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  def update
+    @user.update(user_params)
+    head :no_content
+  end
+
+  def destroy
+    @user.destroy
+    head :no_content
+  end
+
+  def index
+    @users = User.all
+    json_response(@users)
+  end
+
+  def show
+    json_response(@user)
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :clients, dependent: :destroy
+  #validations
+  validates_presence_of :first_name, :last_name, :email, :password
 end
