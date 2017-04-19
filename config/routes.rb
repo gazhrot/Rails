4077b9fd564d6621f12376c8503da2b58c6c6570
gazhrot <axel.bruneaux@epitech.eu>
2017-04-19 15:07:25 +0200

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, path: '/', constraints: { subdomain: 'api' } do
+
     resources :todos do
       resources :items
     end
+
+    resources :users do
+      resources :clients
+      resources :order_histories
+      resources :visit_histories
+    end
+
+    resources :enterprise_users do
+      resources :users
+      resources :enterprises
+    end
+
   end
 end
