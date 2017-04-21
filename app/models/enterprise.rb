62bd33dc5c911ab7cf8aee1e15d14faef4ddff14
gazhrot <axel.bruneaux@epitech.eu>
2017-04-21 20:10:20 +0200

    update model for enterprise -> user relationships

--- app/models/enterprise.rb
+++ app/models/enterprise.rb
@@ -1,2 +1,7 @@
 class Enterprise < ApplicationRecord
-end
+  # model associations
+  has_many :enterprise_users
+  has_many :users, :through => :enterprise_users
+  #validations
+  validates_presence_of :name
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord
   # model associations
   has_many :user_clients
+  has_many :enterprise_users
   has_many :clients, :through => :user_clients
+  has_many :enterprise, :through => :enterprise_users
   #validations
   validates_presence_of :first_name, :last_name, :email, :password
 end
