dd044cb918746796ef7963401cbe19556bda249b
gazhrot <axel.bruneaux@epitech.eu>
2017-04-21 20:01:30 +0200

    update model client/users

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,7 @@
 class Client < ApplicationRecord
+  # model associations
+  has_many :user_clients
+  has_many :users, :through => :user_clients
+  # validations
+  validates_presence_of :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :clients, dependent: :destroy
+  # model associations
+  has_many :user_clients
+  has_many :clients, :through => :user_clients
   #validations
   validates_presence_of :first_name, :last_name, :email, :password
 end
