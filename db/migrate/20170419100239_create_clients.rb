e47902ef073397cea80b1f8fc5e1d3617e4410e4
gazhrot <axel.bruneaux@epitech.eu>
2017-04-21 20:02:46 +0200

    update migrations for clients

--- db/migrate/20170419100239_create_clients.rb
+++ db/migrate/20170419100239_create_clients.rb
@@ -2,7 +2,6 @@ class CreateClients < ActiveRecord::Migration[5.1]
   def change
     create_table :clients do |t|
       t.string :name
-      t.string :name
       t.float :lat
       t.float :lng
       t.date :last_order_date
